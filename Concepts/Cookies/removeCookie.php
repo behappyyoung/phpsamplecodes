d3b248f34eb9e3de49ac2b5b5df3f5ca7f831d62
young <behappydev@gmail.com>
2015-02-13 17:29:06 -0800

    update 2015-02-13-17:29:06 -

--- Concepts/Cookies/removeCookie.php
+++ Concepts/Cookies/removeCookie.php
@@ -6,7 +6,10 @@ setcookie("test", "", time()-3600, '/');
 ?>
 
     <a href="index.php"> show Cookie </a>
-
+    <br/>
+    <script>
+        document.write(document.cookie);
+    </script>
 <?php
 
 echo '<br /><br /><br /> ========== source file ================= <br />';
--- Concepts/Cookies/setCookie.php
+++ Concepts/Cookies/setCookie.php
@@ -5,7 +5,10 @@ setcookie('test', 'testcookoe', time()+3600, '/');
 ?>
 
 <a href="index.php"> show Cookie </a>
-
+<br/>
+<script>
+    document.write(document.cookie);
+</script>
 <?php
 
 echo '<br /><br /><br /> ========== source file ================= <br />';
